8ca272a349626a2192e9ccc5313768dc23d90174
Sergio Barbosa <serarba@hotmail.com>
2013-07-23 23:55:40 -0500

    Initial  commit.

---
+++ app/controllers/furniture_controller.rb
@@ -0,0 +1,17 @@
+#*******************************************************************************
+#
+# furniture_controller.rb
+# Version 1.0a
+#
+# Description: 
+#
+# Revision History
+# ----------------
+# 16Jan13 - SergioBarbosa - Created controller.
+#*******************************************************************************
+class FurnitureController < ActionController::Base
+  protect_from_forgery
+
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LmmfApp::Application.config.secret_token = '19c41bf42934fa381b76ece5e20cc0916c2f674d88b35dcc254ae7049487a25ef889cd8c8e4da2c6934d4c7a6051d5521de50e3ba1e17a27da6fd6873f749e06'
