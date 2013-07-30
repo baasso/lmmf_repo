5b4f50831e50832c3d15b5812a1335e0d1ebca1d
Sergio Barbosa <serarba@hotmail.com>
2013-07-30 00:17:29 -0500

    I renamed a controller.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+#*******************************************************************************
+#
+# furniture_controller.rb
+# Version 1.0a
+#
+# Description: 
+#
+# Revision History
+# ----------------
+# 23Jul13 - SergioBarbosa - Created controller.
+#*******************************************************************************
+class HomeController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ LmmfApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'furniture#index'
+  root :to => 'Home#index'
 
   # See how all your routes lay out with "rake routes"
 
